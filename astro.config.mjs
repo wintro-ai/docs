1bac25bb61b0b92617b75626754e6916361724e9
JulesVR <j.vanreempts@gmail.com>
2024-05-09 23:53:42 +0200

    add https to astro site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'docs.wintro.ai',
+	site: 'https://docs.wintro.ai',
 	// base: 'docs',
 	integrations: [
 		starlight({
