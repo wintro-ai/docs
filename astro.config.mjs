db97edaca118019f71e9c99f5c6640ebf75bf41c
JulesVR <j.vanreempts@gmail.com>
2024-05-09 23:28:16 +0200

    rm jekyll files

---
+++ astro.config.mjs
@@ -0,0 +1,58 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://www.wintro.ai/',
+	integrations: [
+		starlight({
+			title: 'Docs',
+			logo: {
+				src: './src/assets/logo_full.png',
+			},
+			favicon: './src/assets/favicon.ico',
+			customCss: [
+				// Add custom CSS to the docs.
+				'./src/custom.css'
+			],
+			social: {
+				linkedin: 'https://www.linkedin.com/company/wintroai/',
+				// github: 'https://www.github.com/wintro-ai/docs/',
+				rss: 'https://www.wintro.ai/',
+			},
+			sidebar: [
+				{
+					label: 'Intro to Wintro',
+					// autogenerate: { directory: 'intro-to-wintro' }
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'What is Wintro?', link: '/intro-to-wintro/what-is-wintro' },
+					],
+				},
+				{
+					label: 'For Recruiters',
+					autogenerate: { directory: 'recruiters' },
+				},
+				{
+					label: 'For Employees',
+					autogenerate: { directory: 'employees' },
+				},
+				{
+					label: 'Security & Compliance',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'General', link: '/security-compliance/general' },
+						{ label: 'Architecture', link: '/security-compliance/architecture' },
+						{ label: 'Integrations', link: '/security-compliance/integrations' },
+						{ label: 'Cognitive Services', link: '/security-compliance/cognitive-services' },
+						{ label: 'Data Processing', link: '/security-compliance/data-processing' },
+						{ label: 'GDPR', link: '/security-compliance/gdpr' },
+						{ label: 'Data Inquiry', link: '/security-compliance/data-inquiry' },
+						{ label: 'Access Controls', link: '/security-compliance/access-controls' },
+						{ label: 'Secure Development', link: '/security-compliance/secure-development' },
+					]
+				},
+			],
+		}),
+	],
+});
